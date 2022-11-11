6b88b18ea246ba28760fb43823581bf6fa4937b8
9590 <9590@yeah.net>
2022-11-12 00:27:48 +0800

    fix captchaImage bug

--- music-framework/src/main/java/com/guoguoyun/framework/config/SecurityConfig.java
+++ music-framework/src/main/java/com/guoguoyun/framework/config/SecurityConfig.java
@@ -97,7 +97,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
-                .antMatchers("/login", "/register", "/captchaImage","/app/**","/common/upload","/common/provinceList","/common/cityList").anonymous()
+                //.antMatchers("/login", "/register", "/captchaImage","/app/**","/common/upload","/common/provinceList","/common/cityList").anonymous()
+                .antMatchers("/login", "/**/captchaImage").anonymous()
                 .antMatchers(
                         HttpMethod.GET,
                         "/",
