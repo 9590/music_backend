9203d1d2e93dcb1d1739495caf3810dd4fa85869
9590 <9590@yeah.net>
2022-11-12 22:14:56 +0800

    add permitAll for postman

--- music-framework/src/main/java/com/guoguoyun/framework/config/SecurityConfig.java
+++ music-framework/src/main/java/com/guoguoyun/framework/config/SecurityConfig.java
@@ -113,6 +113,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/webjars/**").anonymous()
                 .antMatchers("/*/api-docs").anonymous()
                 .antMatchers("/druid/**").anonymous()
+                // 在这里添加想要测试的方法访问URL，便可使用postman测试
+                .antMatchers("/prod-api/system/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
                 .and()
